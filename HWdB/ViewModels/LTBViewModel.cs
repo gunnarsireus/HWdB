aaf223178baa6ac3124bac1e5e9a53d084455bfc
gunnarsireus <gunnar.sireus@gmail.com>
2015-05-03 21:44:42 +0200

    LTB started

--- HWdB/ViewModels/LTBViewModel.cs
+++ HWdB/ViewModels/LTBViewModel.cs
@@ -16,9 +16,148 @@
             }
         }
 
+        //string _IB0;
+        //public string IB0
+        //{
+        //    get
+        //    {
+        //        return _IB0;
+        //    }
+        //    set
+        //    {
+        //        _IB0 = value;
+        //    }
+        //}
+
+        string _IB1;
+        public string IB1
+        {
+            get
+            {
+                return _IB1;
+            }
+            set
+            {
+                _IB1 = value;
+            }
+        }
+
+        string _IB2;
+        public string IB2
+        {
+            get
+            {
+                return _IB2;
+            }
+            set
+            {
+                _IB2 = value;
+            }
+        }
+
+
+        string _IB3;
+        public string IB3
+        {
+            get
+            {
+                return _IB3;
+            }
+            set
+            {
+                _IB3 = value;
+            }
+        }
+
+
+        string _IB4;
+        public string IB4
+        {
+            get
+            {
+                return _IB4;
+            }
+            set
+            {
+                _IB4 = value;
+            }
+        }
+
+
+        string _IB5;
+        public string IB5
+        {
+            get
+            {
+                return _IB5;
+            }
+            set
+            {
+                _IB5 = value;
+            }
+        }
+
+
+        string _IB6;
+        public string IB6
+        {
+            get
+            {
+                return _IB6;
+            }
+            set
+            {
+                _IB6 = value;
+            }
+        }
+
+
+        string _IB7;
+        public string IB7
+        {
+            get
+            {
+                return _IB7;
+            }
+            set
+            {
+                _IB7 = value;
+            }
+        }
+
+
+        string _IB8;
+        public string IB8
+        {
+            get
+            {
+                return _IB8;
+            }
+            set
+            {
+                _IB8 = value;
+            }
+        }
+
+
+        string _IB9;
+        public string IB9
+        {
+            get
+            {
+                return _IB9;
+            }
+            set
+            {
+                _IB9 = value;
+            }
+        }
+
+
         public LTBViewModel()
         {
              this.ButtonName = "LTB";
+
         }
 
         protected override void OnDispose()
